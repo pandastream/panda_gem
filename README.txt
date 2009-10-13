fdcf6d73c93c9d0a73cf78e9fe9808d386bdd94b
Damien Tanner <damien@new-bamboo.co.uk>
2009-10-13 13:28:22 +0100

    first pass at aws like request signatures

--- README.txt
+++ README.txt
@@ -1,2 +1,6 @@
 gem sources -a http://gems.github.com (you only have to do this once)
 sudo gem install dctanner-panda_gem
+
+require 'lib/panda.rb'
+Panda.connect!('access_key', 'secret_key', 'localhost', 5678)
+Panda.get('/videos')
--- lib/panda/video.rb
+++ lib/panda/video.rb
@@ -1,133 +1,130 @@
-require 'net/http'
-require 'uri'
-require 'yaml'
+require 'rubygems'
+require 'cgi'
+require 'time'
+require 'hmac'
+require 'hmac-sha2'
+require 'base64'
+require 'restclient'
 
 module Panda
   class << self
-    attr_accessor :account_key, :api_domain, :api_port, :default_encoding_profile
-  
-    def api_port
-      @api_port ||= 80
-      @api_port
-    end
-  end
-  
-  class Video
-    attr_accessor :vals
+    attr_writer :rest_client_options
+    attr_reader :connection
     
-    # Deprecated. Use encoding_for_profile instead and check for status == 'success' manually.
-    def find_encoding(profile_title)
-      self.encodings.find { |e| 
-          e.profile_title == profile_title and e.status == 'success'
-        }
-    end
-    
-    def encoding_for_profile(profile_title)
-      self.encodings.find { |e| 
-          e.profile_title == profile_title
-        }
-    end
-    
-    def self.new_with_attrs(vals)
-      video = new
-      video.vals = {}
-      vals.each do |k,v|
-        video.vals[k] = v
-        class_eval "def video.#{k}; @vals[:#{k}]; end"
-      end
+    def connect!(access_key, secret_key, api_host='api.pandastream.com', api_port=80)
+      @access_key = access_key
+      @secret_key = secret_key
+      @api_host = api_host
+      @api_port = api_port
+      @rest_client_options = {}
       
-      # If this is a parent video, turn the encodings into Panda::Video objects
-      if vals[:status] == "original"
-        vals[:encodings].map! do |e|
-          self.new_with_attrs(e[:video])
-        end
-      end
+      # raise Exception.new("You must supply a :secret_key") unless @secret_key
       
-      return video
+      @connection = RestClient::Resource.new(api_url)
     end
     
-    def self.find(token)
-      response = request(:get, "/videos/#{token}")
-      p = self.new_with_attrs(response[:video])
-      return p
+    def api_url
+      "http://#{@api_host}:#{@api_port}"
+    end
+    
+    # NOTE: get params, MUST be given as the params hash, not as part of the url string
+    def get(request_uri, params={})
+      params = authenticate("GET", request_uri, params)
+      puts add_params_to_request_uri(request_uri, params)
+      RestClient::Resource.new(concat_urls(api_url, add_params_to_request_uri(request_uri, params))).get
     end
 
-    def self.create
-      response = request(:post, "/videos")
-      p = self.new_with_attrs(response[:video])
-      return p
+    def post(request_uri, params)
+      params = authenticate("POST", request_uri, params)
+      RestClient::Resource.new(concat_urls(api_url, request_uri)).post(params)
     end
 
-    def self.videos
-      response = request(:get, "/videos")
-      return response[:videos].map {|v| self.new_with_attrs(v[:video]) }
+    def put(request_uri, params)
+      params = authenticate("PUT", request_uri, params)
+      RestClient::Resource.new(concat_urls(api_url, request_uri)).post(params)
     end
 
-    # Makes request to remote service.
-    def self.request(method, path, params={})
-      raise Panda::AccountKeyNotSet if Panda.account_key.nil?
-      params[:account_key] = Panda.account_key
-      path += ".yaml"
-      http = Net::HTTP.new(Panda.api_domain, Panda.api_port)
-  
-      case method
-      when :get
-        response = http.request_get("#{path}?account_key=#{Panda.account_key}")
-      when :post
-        req = Net::HTTP::Post.new(path)
-        req.form_data = params
-        response = http.request(req)
-      end
-  
-      puts "--> #{response.code} #{response.message} (#{response.body.length})"
-      puts response.body
-      handle_response(response)
+    def delete(request_uri, params={})
+      params = authenticate("DELETE", request_uri, params)
+      puts add_params_to_request_uri(request_uri, params)
+      RestClient::Resource.new(concat_urls(api_url, add_params_to_request_uri(request_uri, params))).delete
+    end
+    
+    # From rest-client/lib/restclient/resource.rb
+    
+    def concat_urls(url, suburl)   # :nodoc:
+			url = url.to_s
+			suburl = suburl.to_s
+			if url.slice(-1, 1) == '/' or suburl.slice(0, 1) == '/'
+				url + suburl
+			else
+				"#{url}/#{suburl}"
+			end
+		end
+
+    # Authentication
+
+    def add_params_to_request_uri(request_uri, params)
+      request_uri + '?' + canonical_querystring(params)
     end
 
-    # Handles response and error codes from remote service.
+    def authenticate(verb, request_uri, params={})
+      # supply timestamp and access key if not already provided
+      params["timestamp"] ||= Time.now.iso8601
+      params["access_key"] ||= @access_key
+      # Existing "Signature"? That's gotta go before we generate a new
+      # signature and add it. 
+      params.delete("signature")
 
-    def self.handle_response(response)
-      case response.code.to_i
-        when 200...400
-          YAML.load(response.body)
-        when 401
-          raise(Panda::UnauthorizedAccess.new(response))
-        when 404
-          raise(Panda::ResourceNotFound.new(response))
-        when 500...600
-          raise(Panda::ServerError.new(response))
-        else
-          raise(Panda::PandaError.new(response, "Unknown response code: #{response.code}"))
-      end
+      query_string = canonical_querystring(params)
+      
+      string_to_sign = verb + "\n" + 
+          @api_host.downcase + "\n" +
+          request_uri + "\n" +
+          query_string
+      puts string_to_sign
+      hmac = HMAC::SHA256.new( @secret_key )
+      hmac.update( string_to_sign )
+      # chomp is important!  the base64 encoded version will have a newline at the end
+      signature = Base64.encode64(hmac.digest).chomp 
+
+      params["signature"] = signature
+
+      #order doesn't matter for the actual request, we return the hash
+      #and let client turn it into a url.
+      puts params.inspect
+      return params
+		end
+		
+    # Insist on specific method of URL encoding, RFC3986. 
+    def url_encode(string)
+      # It's kinda like CGI.escape, except CGI.escape is encoding a tilde when
+      # it ought not to be, so we turn it back. Also space NEEDS to be %20 not +.
+      return CGI.escape(string).gsub("%7E", "~").gsub("+", "%20")
     end
+
+    # param keys should be strings, not symbols please. return a string joined
+    # by & in canonical order. 
+    def canonical_querystring(params)
+      # I hope this built-in sort sorts by byte order, that's what's required. 
+      values = params.keys.sort.collect {|key|  [url_encode(key), url_encode(params[key].to_s)].join("=") }
+
+      return values.join("&")
+    end
+
   end
   
-  class PandaError < StandardError; end
-  
-  class ConnectionError < PandaError # :nodoc:
-    attr_reader :response
-
-    def initialize(response, message = nil)
-      @response = response
-      @message  = message
-    end
-
-    def to_s
-      "Failed with #{response.code} #{response.message if response.respond_to?(:message)}"
-    end
-  end
-  
-  class AccountKeyNotSet < PandaError; end
-  
-  # 4xx Client Error
-  class ClientError < ConnectionError; end # :nodoc:
-  
-  # 401 Unauthorized
-  class UnauthorizedAccess < ClientError; end # :nodoc
-  
-  # 404 Not Found
-  class VideoNotFound < ClientError; end # :nodoc:
-
-  # 5xx Server Error
-  class ServerError < ConnectionError; end # :nodoc:
+  # class AccountKeyNotSet < PandaError; end
+  # 
+  # # 4xx Client Error
+  # class ClientError < ConnectionError; end # :nodoc:
+  # 
+  # # 401 Unauthorized
+  # class UnauthorizedAccess < ClientError; end # :nodoc
+  # 
+  # # 404 Not Found
+  # class VideoNotFound < ClientError; end # :nodoc:
+  # 
+  # # 5xx Server Error
+  # class ServerError < ConnectionError; end # :nodoc:
 end
