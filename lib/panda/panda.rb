b2c646e531830dcce98e51479f6b38886abfe350
vivien <vivien@vivien-new-bamboo-mpb.local>
2010-03-11 13:13:32 +0000

    Added Authentication from url

--- lib/panda/panda.rb
+++ lib/panda/panda.rb
@@ -3,19 +3,23 @@ require 'restclient'
 class Panda
   attr_reader :api_host, :api_port, :access_key, :secret_key, :api_version
   
-  def initialize(auth_params={})
-    params = {:api_host => 'api.pandastream.com', :api_port => 80 }.merge(auth_params)
-    
+  DEFAULT_API_PORT=80
+  DEFAULT_API_HOST="api.pandastream.com"
+  
+  def initialize(auth_params)
     @api_version = 2
-    @cloud_id = params["cloud_id"] || params[:cloud_id]
-    @access_key = params["access_key"] || params[:access_key]
-    @secret_key = params["secret_key"] || params[:secret_key]
-    @api_host = params["api_host"] || params[:api_host]
-    @api_port = params["api_port"] || params[:api_port]
-    @prefix = params["prefix_url"] || "v#{@api_version}"
+    
+    if auth_params.class == String
+      init_from_url(auth_params)
+    else
+      init_from_hash(auth_params)
+    end
+    
     @connection = RestClient::Resource.new(api_url)
   end
-  
+
+
+
   def get(request_uri, params={})
     rescue_restclient_exception do
       query = signed_query("GET", request_uri, params)
@@ -41,7 +45,7 @@ class Panda
       body_of @connection[request_uri + '?' + query].delete
     end
   end
-  
+
   def authentication_params(verb, request_uri, params)
     auth_params = {}
     auth_params['cloud_id'] = @cloud_id
@@ -50,43 +54,72 @@ class Panda
     auth_params['signature'] = ApiAuthentication.authenticate(verb, request_uri, @api_host, @secret_key, params.merge(auth_params))
     return auth_params
   end
-  
+
   def signed_query(*args)
     ApiAuthentication.hash_to_query(signed_params(*args))
   end
-  
+
   def signed_params(verb, request_uri, params = {}, timestamp_str = nil)
     auth_params = params
     auth_params['cloud_id']   = @cloud_id
     auth_params['access_key'] = @access_key
     auth_params['timestamp']  = timestamp_str || Time.now.iso8601(6)
-    
+
     params_to_sign = auth_params.reject{|k,v| ['file'].include?(k)}
     auth_params['signature']  = ApiAuthentication.generate_signature(verb, request_uri, @api_host, @secret_key, params_to_sign)
     auth_params
   end
-  
+
   def api_url
     "http://#{@api_host}:#{@api_port}/#{@prefix}"
   end
-  
+
   def setup_bucket(params={})
     granting_params = { :s3_videos_bucket => params[:bucket], :user_aws_key => params[:access_key], :user_aws_secret => params[:secret_key] }
     put("/clouds/#{@cloud_id}.json", granting_params)
   end
-  
+
   private
-  
-  def rescue_restclient_exception(&block)
-    begin 
-      yield 
-    rescue RestClient::Exception => e
-      e.http_body
+
+    def rescue_restclient_exception(&block)
+      begin
+        yield
+      rescue RestClient::Exception => e
+        e.http_body
+      end
     end
-  end
-  
-  # API change on rest-client 1.4
-  def body_of(response)
-    response.respond_to?(:body) ? response.body : response
-  end
+
+    # API change on rest-client 1.4
+    def body_of(response)
+      response.respond_to?(:body) ? response.body : response
+    end
+
+
+    def init_from_url(url)
+      params = url.scan(/^([^:@]+):([^:@]+)@([^:@]+)(:[\d]+)?\/([^:@]+)$/).flatten
+
+      @access_key = params[0]
+      @secret_key = params[1]
+      @cloud_id   = params[4]
+      @api_host   = params[2]
+
+      if params[3]
+        @api_port = params[3][1..-1]
+      else
+        @api_port = DEFAULT_API_PORT
+      end
+      @prefix     = "v#{@api_version}"
+
+    end
+
+    def init_from_hash(hash_params)
+      params      = { :api_host => DEFAULT_API_HOST, :api_port => DEFAULT_API_PORT }.merge(hash_params)
+
+      @cloud_id   = params["cloud_id"]    || params[:cloud_id]
+      @access_key = params["access_key"]  || params[:access_key]
+      @secret_key = params["secret_key"]  || params[:secret_key]
+      @api_host   = params["api_host"]    || params[:api_host]
+      @api_port   = params["api_port"]    || params[:api_port]
+      @prefix     = params["prefix_url"]  || "v#{@api_version}"
+    end
 end
--- spec/panda_spec.rb
+++ spec/panda_spec.rb
@@ -3,74 +3,89 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe Panda do
   before(:each) do
     FakeWeb.allow_net_connect = false
-    @panda = Panda.new({"access_key" => "my_access_key", "secret_key" => "my_secret_key", "api_host" => "myapihost", "api_port" => 85, "cloud_id" => 'my_cloud_id' })
     Time.stub!(:now).and_return(mock("time", :iso8601 => "2009-11-04T17:54:11+00:00"))
   end
+
+  describe "Connected with Hash" do
+    before(:each) do
+      @panda = Panda.new({"access_key" => "my_access_key", "secret_key" => "my_secret_key", "api_host" => "myapihost", "api_port" => 85, "cloud_id" => 'my_cloud_id' })
+    end
+
+    it "should make get request with signed request to panda server" do
+      FakeWeb.register_uri(:get, "http://myapihost:85/v2/videos?timestamp=2009-11-04T17%3A54%3A11%2B00%3A00&signature=CxSYPM65SeeWH4CE%2FLcq7Ny2NtwxlpS8QOXG2BKe4p8%3D&access_key=my_access_key&cloud_id=my_cloud_id", :body => "abc")
+      @panda.get("/videos").should == "abc"
+    end
+
+    it "should make delete request with signed request to panda server" do
+      FakeWeb.register_uri(:delete, "http://myapihost:85/v2/videos/1?timestamp=2009-11-04T17%3A54%3A11%2B00%3A00&signature=t0IYclDXgjZFRYaMf0Gbg%2B5vOqp7q8QQRN8tlQ3bk8Q%3D&access_key=my_access_key&cloud_id=my_cloud_id", :query => {})
+      @panda.delete("/videos/1").should
+      FakeWeb.should have_requested(:delete, "http://myapihost:85/v2/videos/1?timestamp=2009-11-04T17%3A54%3A11%2B00%3A00&signature=t0IYclDXgjZFRYaMf0Gbg%2B5vOqp7q8QQRN8tlQ3bk8Q%3D&access_key=my_access_key&cloud_id=my_cloud_id")
+    end
+
+    it "should create a signed version of the parameters" do
+      signed_params = @panda.signed_params('POST',
+                                           '/videos.json',
+                                           {"param1" => 'one', "param2" => 'two'}
+                                           )
+      signed_params.should == {
+        'access_key' => "my_access_key",
+        'timestamp' => "2009-11-04T17:54:11+00:00",
+        'cloud_id' => 'my_cloud_id',
+        'signature' => 'w66goW6Ve5CT9Ibbx3ryvq4XM8OfIfSZe5oapgZBaUs=',
+        'param1' => 'one',
+        'param2' => 'two'
+      }
+    end
+
+    it "should create a signed version of the parameters without additional arguments" do
+      @panda.signed_params('POST', '/videos.json').should == {
+        'access_key' => "my_access_key",
+        'timestamp' => "2009-11-04T17:54:11+00:00",
+        'cloud_id' => 'my_cloud_id',
+        'signature' => 'TI2n/dsSllxFhxcEShRGKWtDSqxu+kuJUPs335NavMo='
+      }
+    end
+
+
+    it "should create a signed version of the parameters and difficult characters" do
+      signed_params = @panda.signed_params('POST',
+                                           '/videos.json',
+                                           {"tilde" => '~', "space" => ' '}
+                                           )
+      signed_params.should == {
+        'access_key' => "my_access_key",
+        'timestamp' => "2009-11-04T17:54:11+00:00",
+        'cloud_id' => 'my_cloud_id',
+        'signature' => 'w5P9+xPpQpRlweTh0guFYqQOmF+ZuTKXCmaKpUP3sH0=',
+        'tilde' => '~',
+        'space' => ' '
+      }
+    end
+
+    it "should return a json file for every http code" do
+      FakeWeb.register_uri(:get, "http://myapihost:85/v2/videos?timestamp=2009-11-04T17%3A54%3A11%2B00%3A00&signature=CxSYPM65SeeWH4CE%2FLcq7Ny2NtwxlpS8QOXG2BKe4p8%3D&access_key=my_access_key&cloud_id=my_cloud_id", :body => "abc")
+
+      resource = RestClient::Resource.new("http://myapihost:85/v2")
+      RestClient::Resource.stub!(:new).and_return(resource)
+
+      e = RestClient::Exception.new({:body => "abc", :code => 400})
+      e.stub!(:http_body).and_return("abc")
+
+      resource.stub!(:get).and_raise(e)
+
+      panda = Panda.new({"access_key" => "my_access_key", "secret_key" => "my_secret_key", "api_host" => "myapihost", "api_port" => 85, "cloud_id" => 'my_cloud_id' })
+      panda.get("/videos").should == "abc"
+    end
+  end
   
-  
-  it "should make get request with signed request to panda server" do
-    FakeWeb.register_uri(:get, "http://myapihost:85/v2/videos?timestamp=2009-11-04T17%3A54%3A11%2B00%3A00&signature=CxSYPM65SeeWH4CE%2FLcq7Ny2NtwxlpS8QOXG2BKe4p8%3D&access_key=my_access_key&cloud_id=my_cloud_id", :body => "abc")
-    @panda.get("/videos").should == "abc"
-  end
-
-  it "should make delete request with signed request to panda server" do
-    FakeWeb.register_uri(:delete, "http://myapihost:85/v2/videos/1?timestamp=2009-11-04T17%3A54%3A11%2B00%3A00&signature=t0IYclDXgjZFRYaMf0Gbg%2B5vOqp7q8QQRN8tlQ3bk8Q%3D&access_key=my_access_key&cloud_id=my_cloud_id", :query => {})
-    @panda.delete("/videos/1").should
-    FakeWeb.should have_requested(:delete, "http://myapihost:85/v2/videos/1?timestamp=2009-11-04T17%3A54%3A11%2B00%3A00&signature=t0IYclDXgjZFRYaMf0Gbg%2B5vOqp7q8QQRN8tlQ3bk8Q%3D&access_key=my_access_key&cloud_id=my_cloud_id")
-  end
-  
-  it "should create a signed version of the parameters" do
-    signed_params = @panda.signed_params('POST', 
-        '/videos.json', 
-        {"param1" => 'one', "param2" => 'two'}
-    )
-    signed_params.should == {
-      'access_key' => "my_access_key",
-      'timestamp' => "2009-11-04T17:54:11+00:00",
-      'cloud_id' => 'my_cloud_id',
-      'signature' => 'w66goW6Ve5CT9Ibbx3ryvq4XM8OfIfSZe5oapgZBaUs=',
-      'param1' => 'one',
-      'param2' => 'two'
-    }
-  end
-
-  it "should create a signed version of the parameters without additional arguments" do
-    @panda.signed_params('POST', '/videos.json').should == {
-      'access_key' => "my_access_key",
-      'timestamp' => "2009-11-04T17:54:11+00:00",
-      'cloud_id' => 'my_cloud_id',
-      'signature' => 'TI2n/dsSllxFhxcEShRGKWtDSqxu+kuJUPs335NavMo='
-    }
-  end
-
-
-  it "should create a signed version of the parameters and difficult characters" do
-    signed_params = @panda.signed_params('POST',
-        '/videos.json',
-        {"tilde" => '~', "space" => ' '}
-    )
-    signed_params.should == {
-      'access_key' => "my_access_key",
-      'timestamp' => "2009-11-04T17:54:11+00:00",
-      'cloud_id' => 'my_cloud_id',
-      'signature' => 'w5P9+xPpQpRlweTh0guFYqQOmF+ZuTKXCmaKpUP3sH0=',
-      'tilde' => '~',
-      'space' => ' '
-    }
-  end
-  
-  it "should return a json file for every http code" do
-     FakeWeb.register_uri(:get, "http://myapihost:85/v2/videos?timestamp=2009-11-04T17%3A54%3A11%2B00%3A00&signature=CxSYPM65SeeWH4CE%2FLcq7Ny2NtwxlpS8QOXG2BKe4p8%3D&access_key=my_access_key&cloud_id=my_cloud_id", :body => "abc")
-
-     resource = RestClient::Resource.new("http://myapihost:85/v2")
-     RestClient::Resource.stub!(:new).and_return(resource)
-     
-     e = RestClient::Exception.new({:body => "abc", :code => 400})
-     e.stub!(:http_body).and_return("abc")
-     
-     resource.stub!(:get).and_raise(e)
-     
-     panda = Panda.new({"access_key" => "my_access_key", "secret_key" => "my_secret_key", "api_host" => "myapihost", "api_port" => 85, "cloud_id" => 'my_cloud_id' })
-     panda.get("/videos").should == "abc"
+  describe "Connected with a string url" do
+    before(:each) do
+      @panda2 = Panda.new("f4676630-0522-11df-8148-1231350015b1:1ucba5EDJGf9aIl3/ve1klCkRQqiYZ1bzI+Dpvl3@myhost:85/eefa5d2cbe74289a500e62e2789347f3")
+    end
+    
+    it "should make get request" do
+      FakeWeb.register_uri(:get, "http://myhost:85/v2/videos?timestamp=2009-11-04T17%3A54%3A11%2B00%3A00&signature=YcoBrpnRTwpgEpA%2FPO%2FMJr4d94suUqt5acQv67owUew%3D&access_key=f4676630-0522-11df-8148-1231350015b1&cloud_id=eefa5d2cbe74289a500e62e2789347f3", :body => "abc")
+      @panda2.get("/videos").should == "abc"
+    end
   end
 end
