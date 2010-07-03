9f6619d9a61244d46decfcdf90ee54514bb6399b
vivien <vivien@chipiu-mbp.lan>
2010-07-03 22:31:54 +0100

    removed commented code

--- spec/encoding_spec.rb
+++ spec/encoding_spec.rb
@@ -23,12 +23,7 @@ describe Panda::Encoding do
     Panda::Encoding.find_all_by_video_id("123").first.id.should == 456
     
   end
-  
-  # it "should have a video_id to be valid" do
-  #   video = Panda::Encoding.new(:source_url => "http://a.b.com/file.mp4")
-  #   video.valid?.should == false
-  # end
-  
+
   it "should create a encodings" do
     encoding_json = "{\"source_url\":\"http://a.b.com/file.mp4\",\"id\":\"456\"}"
     stub_http_request(:post, /http:\/\/myapihost:85\/v2\/encodings.json/).
@@ -40,16 +35,6 @@ describe Panda::Encoding do
     encoding.id.should == "456" 
   end
   
-  # it "should find by video_id and encoding_id" do
-  #   encoding_json = "{\"abc\":\"efg\",\"id\":\"456\"}"
-  #   
-  #   stub_http_request(:get, /myapihost:85\/v2\/videos\/123\/encodings\/456.json/).to_return(:body => encoding_json)
-  #   
-  #   encoding = Panda::Encoding.find_by(:id => "456", :video_id => "123")
-  #   encoding.id.should == "456"
-  #   
-  # end
-    
   it "should find by encoding_id" do
     encoding_json = "{\"abc\":\"efg\",\"id\":\"456\"}"
     stub_http_request(:get, /myapihost:85\/v2\/encodings\/456.json/).to_return(:body => encoding_json)
