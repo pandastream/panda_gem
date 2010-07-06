eb828f3ba0e152ee4b60b0afc09cc970372f06ff
vivien <vivien@vivien-new-bamboo-mpb.local>
2010-07-06 17:37:40 +0100

    refactoring

--- lib/panda/modules/associations.rb
+++ lib/panda/modules/associations.rb
@@ -5,34 +5,32 @@ module Panda
     end
 
     module ClassMethods
-      
-      def has_many(*property_names)
-        property_names.collect do |name|
-          define_method name do
-            param_id = "#{self.class.name[0..-1].split('::').last.downcase}_id"
-            unless instance_variable_get("@#{name.to_s}")
-              instance_variable_set("@#{name.to_s}",
-                Panda::const_get(name.to_s[0..-2].capitalize)[send(:connection)].send("find_all_by_#{param_id}",send(:id)))
-            end
+
+      def has_many(relation_name)
+        define_method relation_name do
+          param_id = "#{self.class.name[0..-1].split('::').last.downcase}_id"
+          unless instance_variable_get("@#{relation_name.to_s}")
+            instance_variable_set("@#{relation_name.to_s}",
+              Panda::const_get(relation_name.to_s[0..-2].capitalize)[send(:connection)].
+                send("find_all_by_#{param_id}",send(:id)))
           end
         end
       end
 
 
-      def has_one(*property_names)
-        property_names.collect do |name|
-          define_method name do
-            param_id = "#{name.to_s}_id"
-            unless instance_variable_get("@#{name.to_s}")
-              instance_variable_set("@#{name.to_s}",
-                Panda::const_get(name.to_s.capitalize)[send(:connection)].find(send(param_id.to_sym)))
-            end
+      def has_one(relation_name)
+        define_method relation_name do
+          param_id = "#{relation_name.to_s}_id"
+          unless instance_variable_get("@#{relation_name.to_s}")
+            instance_variable_set("@#{relation_name.to_s}",
+              Panda::const_get(relation_name.to_s.capitalize)[send(:connection)].
+                find(send(param_id.to_sym)))
           end
         end
       end
-    
+
       alias :belongs_to :has_one
-    
+
     end
   end
 end
