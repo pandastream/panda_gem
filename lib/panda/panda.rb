3f6f43f59e369b89c97f922167fad3555a33d05d
vivien <vivien@vivien-new-bamboo-mpb.local>
2010-08-04 12:52:21 +0100

    refactoring of Panda class

--- lib/panda/panda.rb
+++ lib/panda/panda.rb
@@ -1,52 +1,33 @@
 require 'restclient'
+require 'forwardable'
 require 'json' unless defined?(ActiveSupport::JSON)
 
 module Panda
   class << self
+    extend Forwardable
+    
     attr_accessor :cloud, :clouds
     attr_writer :connection
 
+    def_delegators :connection, :get, :post, :put, :delete, :api_url, :setup_bucket
+    
     def configure(auth_params=nil, options={})
       @clouds = {}
       @connection = Panda::Connection.new
-      
+
       if auth_params
         connect!(auth_params, options)
       else
         yield @connection
       end
-      
+
       connection.raise_error=true
       connection.format = :hash
       @cloud = Cloud::new(:id => connection.cloud_id)
     end
-    
+
     def connect!(auth_params, options={})
       self.connection = Connection.new(auth_params, options)
-    end
-    
-    def get(request_uri, params={})
-      connection.get(request_uri, params)
-    end
-    
-    def post(request_uri, params={})
-      connection.post(request_uri, params)
-    end
-    
-    def put(request_uri, params={})
-      connection.put(request_uri, params)
-    end
-    
-    def delete(request_uri, params={})
-      connection.delete(request_uri, params)
-    end
-    
-    def setup_bucket(params={})
-      connection.setup_bucket(params)
-    end
-    
-    def authentication_params(*params)
-      raise "Method deprecated. Please use signed_params instead."
     end
 
     def signed_params(verb, request_uri, params = {}, timestamp_str = nil)
@@ -57,9 +38,6 @@ module Panda
       raise "Not connected. Please connect! first." unless @connection
       @connection
     end
-    
-    def api_url
-      connection.api_url
-    end
+
   end
 end
