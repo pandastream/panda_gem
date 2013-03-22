bfa739fe6888c01cd447f572b0e9c0364e52d09d
Vivien Schilis <vivien.schilis@gmail.com>
2013-03-22 14:50:14 +0000

    missing commit

--- lib/panda/modules/associations.rb
+++ lib/panda/modules/associations.rb
@@ -7,8 +7,6 @@ module Panda
     module ClassMethods
 
       def has_one(relation_name)
-        # for example creates : @video ||= VideoScope.new(self)
-
         define_method relation_name do
           param_id = "#{relation_name}_id"
           if instance_var = instance_variable_get("@#{relation_name}")
@@ -17,14 +15,12 @@ module Panda
             @associations ||= []
             @associations << relation_name
             instance_variable_set("@#{relation_name}",
-              Panda::const_get(relation_name.to_s.capitalize).find(send(param_id.to_sym)))
+              self.cloud.send("#{relation_name}s").find(send(param_id.to_sym)))
           end
         end
       end
 
       def has_many(relation_name)
-        # for example creates : @encodings ||= EncodingScope.new(self)
-
         define_method relation_name do
           model_name = "#{relation_name.to_s[0..-2].capitalize}"
           if instance_var = instance_variable_get("@#{relation_name}")
